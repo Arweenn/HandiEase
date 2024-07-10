885f74b02f26e65f2f3e2b938c0bfa3aad38182e
Arweenn <arwendumont25@outlook.fr>
2024-07-10 17:26:46 +0200

    HandiEase - MVP

---
+++ HandiEase/main/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+app_name = 'main'
+
+
+# Modèle pour les articles
+class Article(models.Model):
+    title = models.CharField(max_length=255)
+    link = models.URLField()
+
+
+    def __str__(self):
+        return self.title  # Représentation sous forme de chaîne du titre de l'article
+
+
+# Modèle pour la liste de lecture
+class ReadingList(models.Model):
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    articles = models.ManyToManyField(Article)
+
+
+    def __str__(self):
+        return f'{self.user.username} - {self.article.title}'
---
+++ HandiEase/main/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from .views import main, authentication, readingList
+
+
+urlpatterns = [
+    path('', main.home, name='homepage'),
+    path('dashboard/', main.dashboard_view, name='dashboard'),
+    path('register/', authentication.register_view, name='register_view'),
+    path('login/', authentication.login_view, name='login_view'),
+    path('logout/', authentication.logout_view, name='logout_view'),
+    path('reading_list/', readingList.reading_list, name="reading_list"),
+    path('add_to_reading_list/', readingList.add_to_reading_list, name="add_to_reading_list"),
+    path('remove_from_reading_list/', readingList.remove_from_reading_list, name="remove_from_reading_list"),
+]
